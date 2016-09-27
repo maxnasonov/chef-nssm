c20d959b335a22382057ee860054ec74bf2ed30d
Maxim Nasonov <maxim@nasonov.me>
2016-09-27 18:31:58 +0700

    support installation via choco

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures NSSM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.1'
+version '1.2.2'
 
 supports 'windows'
 
 depends 'windows', '~> 1.0'
+depends 'chocolatey', '~> 1.0'
 
 source_url 'https://github.com/dhoer/chef-nssm' if respond_to?(:source_url)
 issues_url 'https://github.com/dhoer/chef-nssm/issues' if respond_to?(:issues_url)
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,24 +1,33 @@
 if platform?('windows')
-  src = node['nssm']['src']
-  basename = src.slice(src.rindex('/') + 1, src.rindex('.') - src.rindex('/') - 1)
+  if node['nssm']['install_method'] == 'normal'
+    src = node['nssm']['src']
+    basename = src.slice(src.rindex('/') + 1, src.rindex('.') - src.rindex('/') - 1)
 
-  log("nssm_basename=#{basename}")
+    log("nssm_basename=#{basename}")
 
-  windows_zipfile Chef::Config[:file_cache_path] do
-    checksum node['nssm']['sha256']
-    source src
-    action :unzip
-    not_if { ::File.directory?("#{Chef::Config[:file_cache_path]}/#{basename}") }
-  end
+    windows_zipfile Chef::Config[:file_cache_path] do
+      checksum node['nssm']['sha256']
+      source src
+      action :unzip
+      not_if { ::File.directory?("#{Chef::Config[:file_cache_path]}/#{basename}") }
+    end
 
-  system = node['kernel']['machine'] == 'x86_64' ? 'win64' : 'win32'
+    system = node['kernel']['machine'] == 'x86_64' ? 'win64' : 'win32'
 
-  batch 'copy_nssm' do
-    code <<-EOH
-      xcopy "#{Chef::Config[:file_cache_path].tr('/', '\\')}\\#{basename}\\#{system}\\nssm.exe" \
-"#{node['nssm']['install_location']}" /y
-    EOH
-    not_if { ::File.exist?("#{node['nssm']['install_location']}\\nssm.exe".gsub(/%[^%]+%/) { |m| ENV[m[1..-2]] }) }
+    batch 'copy_nssm' do
+      code <<-EOH
+        xcopy "#{Chef::Config[:file_cache_path].tr('/', '\\')}\\#{basename}\\#{system}\\nssm.exe" \
+  "#{node['nssm']['install_location']}" /y
+      EOH
+      not_if { ::File.exist?("#{node['nssm']['install_location']}\\nssm.exe".gsub(/%[^%]+%/) { |m| ENV[m[1..-2]] }) }
+    end
+  elsif node['nssm']['install_method'] == 'choco'
+    node.default['nssm']['install_location'] = "C:\\ProgramData\\Chocolatey\\bin"
+    include_recipe 'chocolatey'
+
+    chocolatey_package 'nssm' do
+      version '2.24.0'
+    end
   end
 else
   log('NSSM can only be installed on Windows platforms!') { level :warn }
